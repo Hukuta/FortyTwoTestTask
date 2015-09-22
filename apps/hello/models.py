2430b9adf144ef54902981f3308c8842285a99e2
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-22 11:29:13 +0300

    ticket:1 model, view, template and initial data

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,4 +1,17 @@
 """My models"""
-#from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    """ information about person """
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    contacts = models.TextField()
+    bio = models.TextField()
+    email = models.EmailField()
+    jabber = models.EmailField()
+    skype = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.first_name + ' ' + self.last_name
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,7 +1,9 @@
 """ Views """
 from django.shortcuts import render_to_response
+from apps.hello.models import Person
 
 
 def index(request):
     """ My Profile Data Page """
-    return render_to_response('index.html')
+    my_data = Person.objects.get(pk=1)
+    return render_to_response('index.html', dict(person=my_data))
