e87dc39890e1c9dffcf9410e62cf38a829c9455d
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-22 18:47:25 +0300

    ticket:3 view

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,8 +1,10 @@
 """ Views """
+import json
 from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render_to_response
-from apps.hello.models import Person
+from django.views.decorators.csrf import csrf_exempt
+from apps.hello.models import Person, Req
 
 
 def index(request):
@@ -11,15 +13,20 @@ def index(request):
     return render_to_response('index.html', dict(person=my_data))
 
 
+@csrf_exempt
 def requests(request):
     """ My Profile Data Page """
     data = {
-        # TODO: implement this view
         'requests': [],
-        'total': 0
+        'total': Req.objects.filter(read=False).count()
     }
     if request.is_ajax():
-        data = serializers.serialize("json", data)
-        return HttpResponse(data, content_type="application/json")
+        read_ids = request.POST.getlist('ids[]')
+        if read_ids:
+            Req.objects.filter(pk__in=read_ids).update(read=True)
+        requests_list = Req.objects.filter(read=False).order_by('pk')[:10]
+        requests_json = serializers.serialize("json", requests_list)
+        data['requests'] = json.loads(requests_json)
+        return HttpResponse(json.dumps(data), content_type="application/json")
     else:
         return render_to_response('requests.html', data)
