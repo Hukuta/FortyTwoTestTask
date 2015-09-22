ca6cf99971423b395ae764be408699e1af739af6
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-22 19:58:22 +0300

    ticket:3 ignore OperationalError on create table Person

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -2,28 +2,28 @@
 from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
+from django.db.utils import OperationalError
 
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Deleting model 'Person'
-        db.delete_table(u'hello_person')
-
-        # Adding model 'Person'
-        db.create_table(u'hello_person', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('date_of_birth', self.gf('django.db.models.fields.DateField')()),
-            ('contacts', self.gf('django.db.models.fields.TextField')()),
-            ('bio', self.gf('django.db.models.fields.TextField')()),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('jabber', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('skype', self.gf('django.db.models.fields.CharField')(max_length=50)),
-        ))
-        db.send_create_signal(u'hello', ['Person'])
+        try:
+            # Adding model 'Person'
+            db.create_table(u'hello_person', (
+                (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+                ('first_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+                ('last_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+                ('date_of_birth', self.gf('django.db.models.fields.DateField')()),
+                ('contacts', self.gf('django.db.models.fields.TextField')()),
+                ('bio', self.gf('django.db.models.fields.TextField')()),
+                ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+                ('jabber', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+                ('skype', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ))
+            db.send_create_signal(u'hello', ['Person'])
+        except OperationalError:
+            pass
 
     def backwards(self, orm):
         # Deleting model 'Person'
