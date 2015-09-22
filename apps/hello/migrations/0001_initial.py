60b43daa9df0a0647e81796610d245460a464718
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-22 19:55:25 +0300

    ticket:3 Deleting model 'Person' before initial migration

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -8,6 +8,9 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Deleting model 'Person'
+        db.delete_table(u'hello_person')
+
         # Adding model 'Person'
         db.create_table(u'hello_person', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
