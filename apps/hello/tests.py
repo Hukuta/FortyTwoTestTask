f43958171dc2c9a3c31c04e8f04f1e4bfeca6b1a
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-23 21:32:38 +0300

    ticket1: Merge branch 'ticket1' (2)

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -72,7 +72,7 @@ class IndexPage(TestCase):
         self.assertTrue('requests' in data)
         self.assertGreaterEqual(data['total'], len(data['requests']))
 
-    def test_template_usage(self):
+    def test_template_usage_requests(self):
         """ Test correct template """
         page = self.client.get(reverse('requests'))
         self.assertTemplateUsed(page, 'requests.html')
@@ -251,95 +251,3 @@ class AdminActions(TestCase):
         """ Test correct template """
         page = self.client.get(reverse('requests'))
         self.assertTemplateUsed(page, 'requests.html')
-
-
-class EditPage(TestCase):
-    """ Test edit Person Profile page  """
-
-    def setUp(self):
-        # create test user
-        self.tester = 'tester'
-        User.objects.create_user(self.tester, 'test@test.com',
-                                 self.tester)
-
-    def test_need_auth(self):
-        """ Only logged in user cat see form """
-        before = self.client.get(reverse('edit_data'))
-        self.client.login(username=self.tester, password=self.tester)
-        after = self.client.get(reverse('edit_data'))
-        form_before = before.context['form']
-        form_after = after.context['form']
-        self.assertIsNone(form_before.initial.get('first_name'))
-        self.assertIsNotNone(form_after.initial.get('first_name'))
-
-    def test_edit_page_data(self):
-        """ Testing initial data from context """
-        self.client.login(username=self.tester, password=self.tester)
-        page = self.client.get(reverse('edit_data'))
-        form = page.context['form']
-        person = Person.objects.get(pk=1)
-        for field in person._meta.get_all_field_names():
-            self.assertEqual(form.initial[field],
-                             getattr(person, field))
-
-    def test_ajax_post(self):
-        """ Testing ajax request saves changes """
-        self.client.login(username=self.tester, password=self.tester)
-        person = Person.objects.get(pk=1)
-        self.client.post(reverse('edit_data'))
-        ajax_post = {
-            'first_name': 'firstname',
-            'last_name': 'lastname',
-            # date mistake
-            'date_of_birth': '1991-01-41',
-            'contacts': 'contacts',
-            'bio': 'bio',
-            # email mistake
-            'email': 'email@email',
-            'jabber': 'email@email.ru',
-            # empty field
-            'skype': '',
-        }
-        self.client.post(reverse('edit_data'), ajax_post)
-        person = Person.objects.get(pk=1)
-        for field in ('date_of_birth', 'email', 'skype'):
-            self.assertNotEqual(getattr(person, field),
-                                ajax_post[field])
-        ajax_post['date_of_birth'] = '1991-01-01'
-        ajax_post['email'] = 'email@email.ru'
-        ajax_post['skype'] = 'skypeid'
-        self.client.post(reverse('edit_data'), ajax_post)
-        person = Person.objects.get(pk=1)
-        for field in ajax_post.keys():
-            value = getattr(person, field)
-            if isinstance(value, date):
-                value = value.strftime("%Y-%m-%d")
-            self.assertEqual(value, ajax_post[field])
-
-    def test_resize_image(self):
-        """ Testing image save """
-        person = Person.objects.get(pk=1)
-        self.assertEqual(person.get_img_url(), '/static/img/no_image.png')
-        photo = open('assets/img/no_image_test.png', 'rb')
-        data = {
-            'first_name': 'firstname',
-            'last_name': 'lastname',
-            'date_of_birth': '1991-01-01',
-            'contacts': 'contacts',
-            'bio': 'bio',
-            'email': 'email@email.ru',
-            'jabber': 'email@email.ru',
-            'skype': 'skypeid'
-        }
-        photo = SimpleUploadedFile(photo.name,
-                                   photo.read())
-        form = ProfileForm(data, dict(photo=photo), instance=person)
-        self.assertTrue(form.is_valid())
-        form.save()
-        person = Person.objects.get(pk=1)
-
-        self.assertNotEqual(person.get_img_url(),
-                            '/static/img/no_image.png')
-        image_resized = Image.open(person.photo)
-        self.assertLessEqual(image_resized.height, 200)
-        self.assertLessEqual(image_resized.width, 200)
