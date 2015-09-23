0bdcff36c1bb7b309dff42d60306ce85112d3b9e
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-23 20:46:04 +0300

    ticket:1 add to admin

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,1 +1,11 @@
-""" Admin CP """
+from django.contrib import admin
+from apps.hello.models import Person
+
+
+class AdminPersons(admin.ModelAdmin):
+    """ Persons model admin  """
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'email',
+                    'jabber', 'skype', 'contacts', 'bio',)
+
+
+admin.site.register(Person, AdminPersons)
