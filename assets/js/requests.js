78874b7fd14c1ad1cf89e30d8b200c1d4829467f
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-24 18:22:42 +0300

    ticket13: javascript update

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -14,13 +14,29 @@ function read_ids() {
 function render_requests_list() {
     var html = '<ul>';
     $.each(last_resp['requests'], function (i, v) {
-        html += '<li>' + v.fields.info + '</li>';
+        html += '<li><label onclick="$(this).find(\'input\').addClass(\'priority-box\')">'
+            + 'Priority <input type="checkbox" data-id="'
+            + v.pk + '" '
+            + (v.fields.priority=='1' ? 'checked="checked"' : '') + '></label> '
+            + v.fields.info + '</li>';
     });
 
     return html + '</ul>';
 }
+function get_ids_with_priority(checked) {
+    var return_ids = [];
+    $(".priority-box").each(function () {
+        if ($(this).prop('checked') == checked)
+            return_ids.push($(this).attr('data-id'))
+    });
+    return return_ids;
+}
 function update_requests_info() {
-    $.post(document.location.href, {ids: read_ids()}, function (resp) {
+    $.post(document.location.href, {
+        ids: read_ids(),
+        priority0: get_ids_with_priority(false),
+        priority1: get_ids_with_priority(true)
+    }, function (resp) {
         last_resp = resp;
         update_title_val(resp['total']);
         $("#requests").html(render_requests_list());
