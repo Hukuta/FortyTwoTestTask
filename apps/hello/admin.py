b910e8b96f118e13d17602990fcb42e7fb0672c0
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-25 10:23:43 +0300

    ticket3: add requests to admin interface

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.hello.models import Person
+from apps.hello.models import Person, Req
 
 
 class AdminPersons(admin.ModelAdmin):
@@ -8,4 +8,10 @@ class AdminPersons(admin.ModelAdmin):
                     'jabber', 'skype', 'contacts', 'bio', 'photo')
 
 
+class AdminRequests(admin.ModelAdmin):
+    """ HTTP Requests log model admin  """
+    list_display = ('info', 'read', 'priority',)
+
+
 admin.site.register(Person, AdminPersons)
+admin.site.register(Req, AdminRequests)
