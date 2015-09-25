6c31d2519e0d58e45ca58823a5f61dfbda3493af
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-09-25 10:13:26 +0300

    ticket5: add photo to admin interface

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -5,7 +5,7 @@ from apps.hello.models import Person
 class AdminPersons(admin.ModelAdmin):
     """ Persons model admin  """
     list_display = ('first_name', 'last_name', 'date_of_birth', 'email',
-                    'jabber', 'skype', 'contacts', 'bio',)
+                    'jabber', 'skype', 'contacts', 'bio', 'photo')
 
 
 admin.site.register(Person, AdminPersons)
