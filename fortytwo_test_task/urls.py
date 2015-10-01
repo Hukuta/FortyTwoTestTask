e724844be7d5430fe64812c51ab2c40a87cf142f
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-10-01 12:40:56 +0300

    ticket5: add logout, change links

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns(
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'', include('apps.hello.urls')),
+    url('^', include('django.contrib.auth.urls')),
     url(r'hello/', include('apps.hello.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
