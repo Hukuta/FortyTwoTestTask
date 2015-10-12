74b1f0f9e5aff2a2750c5d5c698e40e983952447
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-10-12 11:07:06 +0300

    ticket8: +global MANAGE param

--- run_command.sh
+++ run_command.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+MANAGE=django-admin.py
+SETTINGS=fortytwo_test_task.settings
 PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$(SETTINGS) $(MANAGE) print_objects_count 2> $(date +"%m_%d_%Y").dat
