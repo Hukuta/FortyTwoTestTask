9fc498618a1424a9031a61fcf2597e7c821eb869
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-10-12 12:12:20 +0300

    ticket8: show errors

--- run_command.sh
+++ run_command.sh
@@ -1,1 +1,1 @@
-PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$1 $2 print_objects_count 2> $(date +"%m_%d_%Y").dat
+PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$1 $2 print_objects_count #2> $(date +"%m_%d_%Y").dat
