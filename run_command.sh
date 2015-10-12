cd1041ccc93c248cf25a38d98f641705e630839d
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-10-12 11:39:16 +0300

    ticket8: script like makefile

--- run_command.sh
+++ run_command.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cd $1
 PYTHONPATH=$1
-$2 manage.py print_objects_count
-$2 manage.py print_objects_count 2> $(date +"%m_%d_%Y").dat
+MANAGE=django-admin.py
+SETTINGS=fortytwo_test_task.settings
+
+PYTHONPATH=$1 DJANGO_SETTINGS_MODULE=$(SETTINGS) $(MANAGE) print_objects_count
+
+#$2 manage.py print_objects_count 2> $(date +"%m_%d_%Y").dat
